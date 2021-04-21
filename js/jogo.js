29ddbf865c19fb361bc13784b433d91c061760a1
Josiley Cesario <josileycpraxedes@gmail.com>
2021-04-20 21:27:14 -0300

    Cronometro

--- js/jogo.js
+++ js/jogo.js
@@ -1,27 +1,43 @@
+var altura = 0;
+var largura = 0;
+var vidas = 1;
+var tempo = 15;
 
-var altura = 0
-var largura = 0
-var vidas = 1
 
 function ajustaTamanhoPalcoJogo() {
 	altura = window.innerHeight
 	largura = window.innerWidth
-
 	console.log(largura, altura)
 }
 
 ajustaTamanhoPalcoJogo()
 
+/* cronometro controlando o tempo de partido, decrementa a cada 1s
+    se todos os pontos de vida forem perdidos = Game Over
+    senão = ganha a partida
+*/
+var cronometro = setInterval(
+    function(){
+        tempo -= 1
+        if (tempo < 0) {
+            clearInterval(cronometro)   //limpando a memoria do navegador
+            clearInterval(criaMosca)    //limpando a memoria do navegador
+            alert('vit')
+        } else {
+            document.getElementById('cronometro').innerHTML = tempo
+        }
+    }, 1000
+)
+
 function posicaoRandomica() {
+/*  remover mosquito anterior caso exista
+    se clicado, o elemento deve ser removido e nada acontece
+*/
 
-    /*  remover mosquito anterior caso exista
-        se clicado, o elemento deve ser removido e nada acontece
-    */
-
-    /*  controlando os pontos de vidas:
-        se elemento nao for clicado antes da remoção automática, um ponto de vida deve ser removido
-        se as vidas removidas forem maior do que 3 = Game Over 
-    */
+/*  controlando os pontos de vidas:
+    se elemento nao for clicado antes da remoção automática, um ponto de vida deve ser removido
+    se as vidas removidas forem maior do que 3 = Game Over 
+*/
     if (document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove();
         if (vidas > 3) {
@@ -30,17 +46,16 @@ function posicaoRandomica() {
             document.getElementById('vida' + vidas).src="./image/coracao_vazio.png";
             vidas++
         }
-       
     }
 
-    // posição randomica da mosca na tela
+// posição randomica da mosca na tela
 	var posicaoX = Math.floor(Math.random() * largura) - 90
 	var posicaoY = Math.floor(Math.random() * altura) - 90
 	posicaoX = posicaoX < 0 ? 0 : posicaoX
 	posicaoY = posicaoY < 0 ? 0 : posicaoY
 	console.log(posicaoX, posicaoY)
 
-	//criar o elemento html
+//criar o elemento html
 	var mosquito = document.createElement('img')
 	mosquito.src = 'image/mosca.png'
 	mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
@@ -62,7 +77,7 @@ function tamanhoAleatorio() {
 	switch(classe) {
 		case 0:
 			return 'mosquito1'
-		
+
 		case 1:
 			return 'mosquito2'
 
@@ -74,11 +89,11 @@ function tamanhoAleatorio() {
 // lado aleatório da mosca na tela
 function ladoAleatorio(){
 	var classe = Math.floor(Math.random() * 2)
-	
+
 	switch(classe) {
 		case 0:
 			return 'ladoA'
-		
+
 		case 1:
 			return 'ladoB'
 	}
